0e29cb33d9f6c688f8531dc9ad50aa2139c991d6
RosyG <rmga2106@gmail.com>
2018-03-04 22:22:35 -0600

    Accediendo a la API

---
+++ README.md
@@ -0,0 +1,7 @@
+# WEATHER
+- Pedir la localización para obtener los datos del clima del día y lugar actual. Al dar click en predicciones del clima pasa a la siguiente pantalla.
+- Deberá mostrarse los datos de los próximos 7 días obtenidos desde el API.
+- La vista es mobile frendly.
+
+* Consideraciones:
+Ambas AP: Dark Sky y Flickr requieren crear API key.
---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+const urlCall = "https://api.darksky.net/forecast/d9d8bdc793d48b21d618b8a704e40e5f/19.5691954,-99.1290131";
+
+//Formato de URL: https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+const urlApiFlickr = "https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value";
+$(document).ready ( () => {
+
+  fetch( urlCall )
+    .then( data => showHourly(data) )
+    .catch( error => console.error( 'Oooops!' ) );
+
+});
+
+let showHourly = object => {
+  let hourly = object.hourly;
+  console.log(hourly);
+  let iconDay = hourly.icon;
+  let latitude = object.latitude;
+  console.log(latitude);
+  //las próximas varables son arreglos.
+  let temperature = hourly.data.temperature;//temperature es un array.
+  let windSpeed = hourly.data.windSpeed;
+  let uvIndex = hourly.uvIndex;
+  let pressure = hourly.pressure;
+}
